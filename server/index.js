5fe1cf9937d2e0a9bb593550c84a22a2befacfab
Mars_Clarke <marsthecoder@outlook.com>
2020-12-11 16:56:45 -0700

    OOF

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const massive = require("massive");
+const authCtrl = require("./controllers/authController.js");
+
+const PORT = 4000;
+
+const { SESSION_SECRET, CONNECTION_STRING } = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
+}).then((db) => {
+  app.set("db", db);
+  console.log("DB connected Master Nya~");
+});
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET,
+  })
+);
+
+app.post("/auth/register", authCtrl.register);
+
+app.listen(PORT, () => console.log(`Port ${PORT} Ready Master Nya~`));
